8df0163e68294c4e6022198ae768eb04359fe5e3
Inga Kononova <inga.kononova@gmail.com>
2024-10-15 11:13:14 +0300

    I've described components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
 
@@ -7,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Card>
-        <Rectangle />
+        <Rectangle /* src='./images/Rectangle.png' alt='Rectangle image' *//>
         <Title>Headline</Title>
         <Text>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</Text>
         <ButtonsArea>
@@ -22,25 +21,61 @@ function App() {
 export default App;
 
 const Card = styled.div`
-
+  border: 2px;
+  border-radius: 15px;
+  max-width: 300px;
+  max-height: 350px;
+  box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
+  //background: #fff;
 `
 
-const Rectangle = styled.image`
-
+const Rectangle = styled.img`
+  background: url("./images/Rectangle.png");
+  border-radius: 10px;
+  margin: 10px 10px 20px 10px;
+  width: 280px;
+  height: 170px;
 `
 const Title = styled.h1`
   //Inter Bold
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 16px;
+  color: #000;
+  margin-left: 20px;
 `
 
 const Text = styled.p`
   //Inter Medium
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 1.66667;
+  color: #abb3ba;
+  margin: 20px;
 `
 
 const ButtonsArea = styled.div`
+  display: flex;
+  gap: 12px;
+  margin-left: 20px;
+  margin-bottom: 22px;
 
+  button {
+    cursor: pointer;
+  }
 `
 
 const StyledButton = styled.button`
   //Inter Bold
   //Apakšveids gaišai pogai
+  border-radius: 5px;
+  width: 86px;
+  height: 30px;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 10px;
+  line-height: 2;
+  color: #fff;
+  background: #4e71fe;
 `
