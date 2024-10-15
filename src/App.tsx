0d88b313d1023179d73c00f68f188a64f56a3946
Inga Kononova <inga.kononova@gmail.com>
2024-10-15 10:14:21 +0300

    The initial components have been created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        <Rectangle />
+        <Title>Headline</Title>
+        <Text>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</Text>
+        <ButtonsArea>
+          <StyledButton>See more</StyledButton>
+          <StyledButton>Save</StyledButton>
+        </ButtonsArea>
+      </Card>
     </div>
   );
 }
 
 export default App;
+
+const Card = styled.div`
+
+`
+
+const Rectangle = styled.image`
+
+`
+const Title = styled.h1`
+  //Inter Bold
+`
+
+const Text = styled.p`
+  //Inter Medium
+`
+
+const ButtonsArea = styled.div`
+
+`
+
+const StyledButton = styled.button`
+  //Inter Bold
+  //Apakšveids gaišai pogai
+`
